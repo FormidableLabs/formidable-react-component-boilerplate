ecc1d199d19122ea574ecad935b16481e1b05478
Lauren <lauren.eastridge@formidablelabs.com>
2015-07-15 21:25:27 -0700

    fix eslint in demo app

--- demo/app.js
+++ demo/app.js
@@ -1,4 +1,4 @@
-"use strict";
+/*global document*/
 
 import React from "react/addons";
 import BoilerplateComponent from "../src/boilerplate-component";
@@ -8,14 +8,13 @@ const App = React.createClass({
   render() {
     return (
       <div className="demo">
-        < BoilerplateComponent >
-        </ BoilerplateComponent >
+        < BoilerplateComponent />
       </div>
-    )
+    );
   }
 });
 
 const content = document.getElementById("content");
 
-React.render(<App/>, content)
+React.render(<App/>, content);
 
