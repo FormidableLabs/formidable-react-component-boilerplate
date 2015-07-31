231bb2d4af5174f519aae3a7b8e47dc5ee8602ea
Ryan Roemer <ryan.roemer@formidablelabs.com>
2015-07-31 10:12:25 -0700

    Version 0.0.1 - Initial Release

--- dist/boilerplate-component.js
+++ dist/boilerplate-component.js
@@ -56,24 +56,14 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	"use strict";
 	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-	
-	var _componentsBoilerplateComponent = __webpack_require__(1);
-	
-	var _componentsBoilerplateComponent2 = _interopRequireDefault(_componentsBoilerplateComponent);
-
-	exports["default"] = _componentsBoilerplateComponent2["default"];
-	module.exports = exports["default"];
+	module.exports = {
+	  BoilerplateComponent: __webpack_require__(1)
+	};
 
 /***/ },
 /* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
-	/*eslint-disable: no-unused-vars*/
 	"use strict";
 	
 	Object.defineProperty(exports, "__esModule", {
@@ -82,19 +72,25 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 	
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 	
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 	
-	var _react = __webpack_require__(2);
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
 	
-	/*eslint-enable: no-unused-vars*/
+	var _react = __webpack_require__(2);
 	
 	var _react2 = _interopRequireDefault(_react);
 	
-	var BoilerplateComponent = (function () {
+	var BoilerplateComponent = (function (_React$Component) {
+	  _inherits(BoilerplateComponent, _React$Component);
+	
 	  function BoilerplateComponent() {
 	    _classCallCheck(this, BoilerplateComponent);
+	
+	    _get(Object.getPrototypeOf(BoilerplateComponent.prototype), "constructor", this).apply(this, arguments);
 	  }
 	
 	  _createClass(BoilerplateComponent, [{
@@ -103,13 +99,13 @@ return /******/ (function(modules) { // webpackBootstrap
 	      return _react2["default"].createElement(
 	        "div",
 	        null,
-	        "Edit me in src/components/boilerplate-component.js"
+	        "Edit me!"
 	      );
 	    }
 	  }]);
 	
 	  return BoilerplateComponent;
-	})();
+	})(_react2["default"].Component);
 	
 	exports["default"] = BoilerplateComponent;
 	module.exports = exports["default"];
